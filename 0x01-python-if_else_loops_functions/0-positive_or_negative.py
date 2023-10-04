01c27b23ea532adbfe898019e3035921c52a86b1
Steve <ustinsteve@gmail.com>
2023-10-04 17:21:52 +0100

    worked on elif statement in solving +ve, -ve or zero

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -4,6 +4,6 @@ number = random.randint(-10, 10)
 if number > 0:
     print("{} is positive".format(number))
 elif number == 0:
-    print(f"{number} is 0")
+    print(f"{number} is zero")
 else:
     print("{0} is negative".format(number))
